c1e793c1061165df2c5b179e5cbd28c9a9c59f10
Rifat Fachrial Farhan <140954647+Circlecraft@users.noreply.github.com>
2025-04-16 11:55:25 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:gws_start/landing_screen.dart';
-import 'package:gws_start/login_screen.dart';
-import 'package:gws_start/registration_screen.dart';
+import 'package:getwellsoon_apk/pages/article_screen.dart';
+import 'package:getwellsoon_apk/pages/journal_log_screen.dart';
+import 'package:getwellsoon_apk/pages/landing_screen.dart';
+import 'package:getwellsoon_apk/pages/login_screen.dart';
+import 'package:getwellsoon_apk/pages/meditation_screen.dart';
+import 'package:getwellsoon_apk/pages/mood_tracker_screen.dart';
+import 'package:getwellsoon_apk/pages/music_screen.dart';
+import 'package:getwellsoon_apk/pages/registration_screen.dart';
 
 void main() {
-  runApp(const MentalHealthApp());
+  runApp(const MyApp());
 }
 
-class MentalHealthApp extends StatelessWidget {
-  const MentalHealthApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Mental Health App',
       debugShowCheckedModeBanner: false,
-      title: 'Get Well Soon',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialRoute: '/registration',
+      theme: AppTheme.lightTheme,
+      initialRoute: '/login',
       routes: {
-        '/registration': (context) => const RegistrationScreen(),
+        '/': (context) => const LoginScreen(),
         '/login': (context) => const LoginScreen(),
+        '/registration': (context) => const RegistrationScreen(),
         '/landing': (context) => const LandingScreen(),
+        '/mood_tracker': (context) => const MoodTrackerScreen(),
+        '/journal_log': (context) => const JournalLogScreen(),
+        '/articles': (context) => const ArticlesScreen(),
+        '/meditation': (context) => const MeditationScreen(),
+        '/music': (context) => const MusicScreen(),
       },
     );
   }
+}
+
+class AppTheme {
+  static var lightTheme;
 }
